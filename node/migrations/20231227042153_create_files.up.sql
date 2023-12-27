d59d2f43aa958926b595dc5d19861d15f0358fd0
Charles Bassett <chasb96@gmail.com>
2023-12-26 23:52:03 -0600

    Increase space for file names

--- node/migrations/20231227042153_create_files.up.sql
+++ node/migrations/20231227042153_create_files.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE files (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(64) UNIQUE NOT NULL,
+    name VARCHAR(256) UNIQUE NOT NULL,
     content_type VARCHAR(128) NOT NULL,
     extension VARCHAR(8),
     internal_name VARCHAR(64) NOT NULL
